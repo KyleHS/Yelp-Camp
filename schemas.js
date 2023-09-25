f8507c8dd21265ba7275376ebcf03382be725f2f
KyleHS <k15156@yahoo.com>
2023-09-25 14:00:40 -0600

    added middleware for deleting reviews

--- schemas.js
+++ schemas.js
@@ -11,10 +11,8 @@ module.exports.campgroundSchema = Joi.object({
 });
 
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
         review: Joi.object({
             rating: Joi.number().required(),
             body: Joi.string().required()
-        })
     }).required()
 });
