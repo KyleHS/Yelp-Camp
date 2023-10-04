2a171683a4ef3a2d881fce143f34728cdcf17eba
KyleHS <k15156@yahoo.com>
2023-10-04 11:42:25 -0600

    Created a reviews file within routes.  Moved routes revolved around user reviews to the new file.  Removed reviews routes from app.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const catchAsync = require('../utils/catchAsync');
+
 const { campgroundSchema } = require('../schemas.js');
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
+
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
 
 const validateCampground = (req, res, next) => {
  
