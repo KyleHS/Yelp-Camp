5a8d3d469fc274e4efa929e63b8876e3de0b5cf5
KyleHS <k15156@yahoo.com>
2024-06-04 11:45:10 -0600

    Installed passport-local-mongoose to the project and added in notes explaining the purpose of this

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose'); //Link passport to user model
 
 const UserSchema = new Schema({
-    email: {
+    email: { //We don't need to add a password here as passport covers that
         type: String,
         required: true,
         unique: true
@@ -11,5 +11,7 @@ const UserSchema = new Schema({
 });
 
 UserSchema.plugin(passportLocalMongoose);
+//Adds username, hash, and salt field to store username, hashed password
+//and the salt value
 
 module.exports = mongoose.model('User', UserSchema);
